e4e2c1c79c973afa67d44b69396bd02fd07a6d0d
Aliakbarmfe <aliakbarmfe@gmail.com>
2026-08-16 21:37:41 +0500

    Create firebase-config.js

---
+++ api/firebase-config.js
@@ -0,0 +1,11 @@
+export default function handler(req, res) {
+  res.status(200).json({
+    apiKey: "AIzaSyClJLVE8SbUCoE-TaQXD77kELheELK6FCo",
+    authDomain: "fizongame-a1209.firebaseapp.com",
+    databaseURL: "https://fizongame-a1209-default-rtdb.firebaseio.com",
+    projectId: "fizongame-a1209",
+    storageBucket: "fizongame-a1209.firebasestorage.app",
+    messagingSenderId: "360630309827",
+    appId: "1:360630309827:web:43328c490a0713e760e376"
+  });
+}
